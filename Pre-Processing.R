2535694d665693c25d64b20b7e4172385607b8d0
neokok <neokok@umich.edu>
2025-03-12 16:07:44 -0400

    Combined trial variables to data

--- Pre-Processing.R
+++ Pre-Processing.R
@@ -49,5 +49,36 @@ if (!dir.exists("csv_data")) {
 }
 
 # Save the processed dataset to a CSV file in the 'csv_data' folder
-final_data %>% write.csv("csv_data/brown2021.csv", row.names = FALSE)
+#final_data %>% write.csv("csv_data/brown2021.csv", row.names = FALSE)
 
+roster = as.data.frame(
+  read.table(paste0("DCLP3 Public Dataset - Release 3 - 2022-08-04/Data Files/PtRoster_a.txt"),
+             sep = "|", header = TRUE, fileEncoding = "UTF-16LE", stringsAsFactors = FALSE, na.strings = "") %>% rename(id = PtID)
+)
+
+visit = as.data.frame(
+  read.table(paste0("DCLP3 Public Dataset - Release 3 - 2022-08-04/Data Files/VisitInfo_a.txt"),
+             sep = "|", header = TRUE, fileEncoding = "UTF-16LE", stringsAsFactors = FALSE, na.strings = "") %>% rename(id = PtID)
+)
+# Separate randomization and screening visits
+visit_randomization = visit %>% filter(Visit == "Randomization") %>% select(id, randomization = VisitDt)
+visit_screening = visit %>% filter(Visit == "Run-in Initiation") %>% select(id, screening = VisitDt)
+
+data = final_data %>% left_join(roster, by = c("id")) %>% # Join "roster" df to identify treatment type
+  select(id, time, gl, screening = EnrollDt, randomization = RandDt, site = SiteID, trt = trtGroup) %>% # Select important variables
+  mutate(screening = as.POSIXct(screening),
+         randomization = as.POSIXct(randomization)) # Change screening and randomization into date format
+
+phase_trial = data %>% mutate(phase = case_when(time < randomization & time >= randomization - weeks(2) ~ "baseline",
+                                                time >= randomization & time < randomization + weeks(26) ~ "trial")) # Identify last 2 weeks of baseline and first 13 weeks of trial
+
+df = phase_trial %>% filter(phase == "trial")
+df_base = phase_trial %>% filter(phase == "baseline")
+
+comparison_trial = df_base %>% filter(phase == "baseline") %>% # Only include trial phase
+  in_range_percent() %>% left_join(data, by = c("id")) %>% # Calculate TIR and merge back with data df to get all variables
+  group_by(trt) %>% summarise(mean = mean(in_range_70_180), sd = sd(in_range_70_180), n = length(unique(id))) # group by treatment and summarise
+
+
+
+write_csv(phase_trial, "cleaned_data.csv")
