eb0f2bcaec1aca8912f011364d38d3e1781a397f
walterwilliamson <waltw@umich.edu>
2025-03-20 12:28:33 -0400

    added time below range, changed episode to rate, and changed window to 182 days following original study design

--- analysis.R
+++ analysis.R
@@ -16,6 +16,7 @@ baseline_data <- data |> filter(phase == "baseline")
 baseline_metrics <- cbind(in_range_percent(baseline_data, 
                                            target_ranges = list(c(70,180))),
                           cv_glu(baseline_data)[,2],
+                          below_percent(baseline_data, targets_below = c(70))[,2],
                           episode_calculation(baseline_data) |> 
                             filter(type == "hypo", level == "lv1") |> 
                             select(total_episodes),
@@ -29,6 +30,7 @@ trial_metrics <- function(data, window){
   
   metrics <- cbind(in_range_percent(data, target_ranges = list(c(70,180))),
         cv_glu(data)[,2],
+        below_percent(data, targets_below = c(70))[,2],
         episode_calculation(data) |> 
           filter(type == "hypo", level == "lv1") |> 
           select(total_episodes),
@@ -39,7 +41,7 @@ trial_metrics <- function(data, window){
   return(metrics)
   }
 
-windows <- c(7,14,30,60,90,180)
+windows <- c(7,14,30,60,90,182)
 
 for(i in seq_along(windows)){
   assign(paste0("trial_",windows[i]), trial_metrics(trial_data, windows[i]))
@@ -52,10 +54,21 @@ metrics_all <- rbind(baseline_metrics,
                      trial_30,
                      trial_60,
                      trial_90,
-                     trial_180) 
+                     trial_182) 
 
 metrics_all <- metrics_all |> 
-  rename(tot_hyper_ep = total_episodes)
+  left_join(data |> 
+              group_by(id) |> 
+              summarise(b_end = max(time), b_start = min(time), 
+                        site = first(site), trt = first(trt),
+                        randomization = first(randomization),
+                        screening = first(screening),
+                        .groups = "drop" ) |>
+              select(id, site, trt, b_end, b_start, randomization, screening), by = "id") |> 
+  rename(tot_hyper_ep = total_episodes) |> 
+  mutate(randomization_days = case_when(window != "baseline" ~ as.numeric(window),
+                                   window == "baseline" ~ round(as.numeric(difftime(randomization, b_start, units = "days")))),
+         episode_rate = tot_hyper_ep/randomization_days) |> 
+  select(-randomization_days)
 
 write_csv(metrics_all, "data/metrics_all.csv")
-
